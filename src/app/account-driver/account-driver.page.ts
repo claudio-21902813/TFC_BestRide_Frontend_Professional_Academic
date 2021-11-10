d31f23c055ca3b124beec0ce5006218877430c6f
Nuno Caetano <a21902813@alunos.ulht.pt>
2021-11-10 17:58:16 +0000

    err fix

--- src/app/account-driver/account-driver.page.ts
+++ src/app/account-driver/account-driver.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder} from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
@@ -8,46 +8,42 @@ import { Router } from '@angular/router';
   styleUrls: ['./account-driver.page.scss'],
 })
 export class AccountDriverPage implements OnInit {
-
   ionicForm: FormGroup;
 
-  constructor(
-    public formBuilder: FormBuilder,
-    private router: Router,
-  ) { }
+  constructor(public formBuilder: FormBuilder, private router: Router) {}
 
   ngOnInit() {
     this.ionicForm = this.formBuilder.group({
-      name: ['Test First and Last name'],
+      name: [Validators.required, 'Test First and Last name'],
       email: ['driver@best'],
       gender: ['m'],
       guide: ['elder'],
       languages: ['Portuguese'],
-      phone:['919345678'],
-      address:['123 Test Street'],
-      postal:['1749-024 Lisboa'],
-      country:['Portugal'],
-      city:['Lisboa'],
-      compName:['BestRide Comp'],
-      workAddress:['468 Test Street'],
-      workPhone:['936658909'],
-      originCountry:['Portugal'],
-      vehicles:['Tuk Tuk'],
-      availabilityHoursSince:['09:00Z'],
-      availabilityHoursUntil:['22:00Z'],
-      nif:['230456133'],
-      nameCourse:['Turism Course 2002'],
-      driverLicense:['LL-235363 0'],
-      ancat:['157678901'],
-      rnaat:['123768539'],
-      iban:['PT50 2345 102030 23678690'],
-      emergencyName:['Name Emergency'],
-      emergencyPhone:['936789321'],
-      relation:['Father'],
-      type:['Guide'],
-      about:['Small Text about me'],
-      link:['https://www.youtube.com/2Xt56'],
-      dateActivity:['17 January 2010'],
+      phone: ['919345678'],
+      address: ['123 Test Street'],
+      postal: ['1749-024 Lisboa'],
+      country: ['Portugal'],
+      city: ['Lisboa'],
+      compName: ['BestRide Comp'],
+      workAddress: ['468 Test Street'],
+      workPhone: ['936658909'],
+      originCountry: ['Portugal'],
+      vehicles: ['Tuk Tuk'],
+      availabilityHoursSince: ['09:00Z'],
+      availabilityHoursUntil: ['22:00Z'],
+      nif: ['230456133'],
+      nameCourse: ['Turism Course 2002'],
+      driverLicense: ['LL-235363 0'],
+      ancat: ['157678901'],
+      rnaat: ['123768539'],
+      iban: ['PT50 2345 102030 23678690'],
+      emergencyName: ['Name Emergency'],
+      emergencyPhone: ['936789321'],
+      relation: ['Father'],
+      type: ['Guide'],
+      about: ['Small Text about me'],
+      link: ['https://www.youtube.com/2Xt56'],
+      dateActivity: ['17 January 2010'],
     });
   }
 
@@ -65,12 +61,9 @@ export class AccountDriverPage implements OnInit {
     this.router.navigate(['/home']);
   }
 
-  public alterarPass(): void {
-
-  }
+  public alterarPass(): void {}
 
   public deleteAccount(): void {
     //this.dadosContaApi.deleteUser();
   }
-
 }
